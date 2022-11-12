a058fcc3075d4e472c6d6a8667f69d188998a47f
ravanmaxmud <ravanim@code.edu.az>
2022-11-12 22:15:39 +0400

    Aded Controllers

---
+++ AdminPanel/AdminPanel/Controllers/CyrptoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    public class CyrptoController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
