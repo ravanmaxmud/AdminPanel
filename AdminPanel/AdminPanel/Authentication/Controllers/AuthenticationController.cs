7ee0cd2f368e62ea815dc40ea2efcfeb569f5232
ravanmaxmud <ravanim@code.edu.az>
2022-11-13 12:59:32 +0400

    Aded Authentication Folder Structure

---
+++ AdminPanel/AdminPanel/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Authentication.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
