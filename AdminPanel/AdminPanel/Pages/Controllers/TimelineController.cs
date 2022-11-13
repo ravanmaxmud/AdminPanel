a24eb846519b92636a7dd9d8a9258a5b6da4d396
ravanmaxmud <ravanim@code.edu.az>
2022-11-13 12:19:27 +0400

    Aded Timeline Controller

---
+++ AdminPanel/AdminPanel/Pages/Controllers/TimelineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Pages.Controllers
+{
+    public class TimelineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
